98ae63e5a09504c111eb032555d5c1b7ea632dd9
mohamed kamal <mkahf5103@gmail.com>
2025-05-02 18:39:59 +0400

    Add project Structure

---
+++ Ecommerce.core/Entities/Product/Photo.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Ecommerce.core.Entities.Product
+{
+    public class Photo:BaseEntity<int>
+    {
+        public string ImageName { get; set; }
+        public int ProductId { get; set; }
+        [ForeignKey(nameof(ProductId))]
+        public virtual Product Product { get; set; }
+    }
+}
--- Ecommerce.core/Entities/Product/Product.cs
+++ Ecommerce.core/Entities/Product/Product.cs
@@ -7,6 +7,7 @@ namespace Ecommerce.core.Entities.Product
         public string Name { get; set; }
         public string? Description { get; set; }
         public decimal Price { get; set; }
+        public virtual List<Photo> Photos { get; set; }
         public int CategoryId { get; set; }
         [ForeignKey(nameof(CategoryId))]
         public virtual Category Category { get; set; }
